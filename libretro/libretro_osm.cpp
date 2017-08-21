8ef5d7104d6ae405c8486a84ef4e6b8ab32cc7f3
twinaphex <libretro@gmail.com>
2017-08-21 03:17:08 +0200

    More build fixes

--- libretro/libretro_osm.cpp
+++ libretro/libretro_osm.cpp
@@ -2,12 +2,13 @@
 
 OnScreenMessages osm;
 
-void OnScreenMessages::Show(const std::string &message, float duration_s, uint32_t color, int icon, bool checkUnique) {
+void OnScreenMessages::Show(const std::string &message, float duration_s, uint32_t color, int icon, bool checkUnique, const char *id) {
 	(void)message;
    (void)duration_s;
    (void)color;
    (void)icon;
    (void)checkUnique;
+   (void)id;
 }
 
 void OnScreenMessages::ShowOnOff(const std::string &message, bool b, float duration_s, uint32_t color, int icon) {
