c43ee7bb628a7b36e4a88314702932e7f2b65e0d
twinaphex <libretro@gmail.com>
2017-08-29 15:43:45 +0200

    Sound works now - had to set iGlobalVolume

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -431,6 +431,7 @@ static void check_variables(void)
    var.key = "ppsspp_internal_resolution";
    var.value = NULL;
 
+   g_Config.iGlobalVolume = VOLUME_MAX;
    g_Config.bEnableSound  = true;
 
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
