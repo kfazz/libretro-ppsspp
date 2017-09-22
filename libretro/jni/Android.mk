a65eb856e2adf35651558362a29e5966cbf7bc37
Aaron Kling <webgeek1234@gmail.com>
2017-09-22 12:39:27 -0500

    Fix Android compile for non arm platforms

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -1,91 +1,85 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 ifneq ($(GIT_VERSION)," unknown")
-	LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+	COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
 
-LOCAL_MODULE    := libavformat
-CORE_DIR        := ../..
-FFMPEGDIR       := $(CORE_DIR)/ffmpeg
-FFMPEGLIBDIR    := $(FFMPEGDIR)/android/armv7/lib
-LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavformat.a
+COREFLAGS  :=
+CORE_DIR   := ../..
+FFMPEGDIR  := $(CORE_DIR)/ffmpeg
+FFMPEGLIBS += libavformat libavcodec libavutil libswresample libswscale
 
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libavcodec
-LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavcodec.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libavutil
-LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavutil.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libswresample
-LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libswresample.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libswscale
-LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libswscale.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
+ifeq ($(TARGET_ARCH),arm64)
+  COREFLAGS += -DARM64 -D_ARCH_64
+  HAVE_NEON := 1
+  WITH_DYNAREC = arm64
+  FFMPEGLIBDIR := $(FFMPEGDIR)/android/arm64/lib
+  FFMPEGINCFLAGS := -I$(FFMPEGDIR)/android/arm64/include
+endif
 
 ifeq ($(TARGET_ARCH),arm)
-  LOCAL_CFLAGS += -DANDROID_ARM -DARM -DARMEABI_V7A
-  LOCAL_ARM_MODE := arm
-ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-  LOCAL_ARM_NEON := true
-endif
-  LOCAL_CLFAGS += -marm
+  COREFLAGS += -DARM -DARMEABI_V7A -D__arm__ -DARM_ASM -D_ARCH_32 -mfpu=neon
+  HAVE_NEON := 1
+  WITH_DYNAREC = arm
+  FFMPEGLIBDIR := $(FFMPEGDIR)/android/armv7/lib
+  FFMPEGINCFLAGS := -I$(FFMPEGDIR)/android/armv7/include
 endif
 
 ifeq ($(TARGET_ARCH),x86)
-  LOCAL_CFLAGS += -DANDROID_X86 -D_M_IX86 -fomit-frame-pointer -mtune=atom -mfpmath=sse -mssse3
+  COREFLAGS += -D_ARCH_32 -D_M_IX86 -fomit-frame-pointer -mtune=atom -mfpmath=sse -mssse3 -mstackrealign
+  WITH_DYNAREC = x86
+  FFMPEGLIBDIR := $(FFMPEGDIR)/android/x86/lib
+  FFMPEGINCFLAGS := -I$(FFMPEGDIR)/android/x86/include
 endif
 
-ifeq ($(TARGET_ARCH),mips)
-  LOCAL_CFLAGS += -DANDROID_MIPS
+ifeq ($(TARGET_ARCH),x86_64)
+  COREFLAGS += -D_ARCH_64 -D_M_X64 -fomit-frame-pointer -mtune=atom -mfpmath=sse -mssse3 -mstackrealign
+  WITH_DYNAREC = x86_64
+  FFMPEGLIBDIR := $(FFMPEGDIR)/android/x86_64/lib
+  FFMPEGINCFLAGS := -I$(FFMPEGDIR)/android/x86_64/include
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libavformat
+LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavformat.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libavcodec
+LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavcodec.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libavutil
+LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libavutil.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libswresample
+LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libswresample.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libswscale
+LOCAL_SRC_FILES := $(FFMPEGLIBDIR)/libswscale.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 
 PLATFORM_EXT   := android
-
-FFMPEGINCFLAGS := -I$(FFMPEGDIR)/android/armv7/include
-FFMPEGLIBDIR   := $(FFMPEGDIR)/android/armv7/lib
-FFMPEGLIBS     += libavformat libavcodec libavutil libswresample libswscale
-
-WITH_DYNAREC = arm
+platform := android
 GLES = 1
-
-INCFLAGS += $(FFMPEGINCFLAGS)
 
 LOCAL_MODULE := retro
 
-platform := android
-HAVE_NEON := 1
-
 include $(CORE_DIR)/libretro/Makefile.common
 
-COREFLAGS := -DINLINE="inline" -DPPSSPP -DUSE_FFMPEG -DMOBILE_DEVICE -DBAKE_IN_GIT -DDYNAREC -D__LIBRETRO__ -D__arm__ -DARM_ASM -D__NEON_OPT__ -DUSING_GLES2 -D__STDC_CONSTANT_MACROS -D_ARCH_32 -DGLEW_NO_GLU -DNO_VULKAN $(INCFLAGS)
+COREFLAGS += -DINLINE="inline" -DPPSSPP -DUSE_FFMPEG -DMOBILE_DEVICE -DBAKE_IN_GIT -DDYNAREC -D__LIBRETRO__ -DUSING_GLES2 -D__STDC_CONSTANT_MACROS -DGLEW_NO_GLU -DNO_VULKAN $(INCFLAGS)
 LOCAL_SRC_FILES = $(SOURCES_CXX) $(SOURCES_C) $(ASMFILES)
 LOCAL_CPPFLAGS := -Wall -std=gnu++11 -Wno-literal-suffix $(COREFLAGS)
 LOCAL_CFLAGS := -O2 -ffast-math -DANDROID $(COREFLAGS)
-LOCAL_LDLIBS += -lz -llog -lGLESv2 -lEGL
+LOCAL_LDLIBS += -lz -llog -lGLESv2 -lEGL -latomic
 LOCAL_STATIC_LIBRARIES += $(FFMPEGLIBS)
 
 include $(BUILD_SHARED_LIBRARY)
